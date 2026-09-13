da1dea40551f5f93876b1bcff0a0b95549550512
wilmer jimenez <108029465+WilmerDep@users.noreply.github.com>
2026-09-12 23:56:19 -0400

    feat(services): load ordering and revalidation

--- wp-headless-api-core.php
+++ wp-headless-api-core.php
@@ -41,7 +41,9 @@ require_once HEADLESS_API_CORE_PATH . 'modules/Directory/Directory_Module.php';
 require_once HEADLESS_API_CORE_PATH . 'modules/Services/Services_Post_Type.php';
 require_once HEADLESS_API_CORE_PATH . 'modules/Services/Services_Serializer.php';
 require_once HEADLESS_API_CORE_PATH . 'modules/Services/Services_Controller.php';
+require_once HEADLESS_API_CORE_PATH . 'modules/Services/Services_Order.php';
 require_once HEADLESS_API_CORE_PATH . 'modules/Services/Services_Admin.php';
+require_once HEADLESS_API_CORE_PATH . 'modules/Services/Services_Revalidation.php';
 require_once HEADLESS_API_CORE_PATH . 'modules/Services/Services_Module.php';
 
 add_action( 'plugins_loaded', array( 'HeadlessApiCore\\Core\\Plugin', 'boot' ) );
