9f8b44e7ffc4a2fcfe324879b93ac6cd06021af6
wilmer jimenez <108029465+WilmerDep@users.noreply.github.com>
2026-09-15 01:31:56 -0400

    feat(forms): register mail template test delivery

--- modules/Forms/Forms_Module.php
+++ modules/Forms/Forms_Module.php
@@ -27,16 +27,18 @@ final class Forms_Module {
 			return;
 		}
 
-		$post_type  = new Forms_Post_Type();
-		$serializer = new Forms_Serializer( $mail_settings );
-		$validator  = new Forms_Validator();
-		$renderer   = new Mail_Template_Renderer();
-		$submission = new Forms_Submission( $mail_settings, $validator, $renderer );
-		$controller = new Forms_Controller( $serializer, $submission );
-		$admin      = new Forms_Admin();
+		$post_type    = new Forms_Post_Type();
+		$serializer   = new Forms_Serializer( $mail_settings );
+		$validator    = new Forms_Validator();
+		$renderer     = new Mail_Template_Renderer();
+		$template_test = new Mail_Template_Test( $mail_settings, $renderer );
+		$submission   = new Forms_Submission( $mail_settings, $validator, $renderer );
+		$controller   = new Forms_Controller( $serializer, $submission );
+		$admin        = new Forms_Admin();
 
 		$post_type->register();
 		$controller->register();
 		$admin->register();
+		$template_test->register();
 	}
 }
